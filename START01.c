7deb8e6efcb02400e22de45774b2a07ce4bf0e19
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-03-23 13:18:44 +0200

    Create START01.c

---
+++ START01.c
@@ -0,0 +1,14 @@
+//START01 
+
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int a;
+  scanf("%d", &a);
+  printf("%d", a);
+}
